eed83c713acda73972738e0ec878fd06ec1bf1c4
Vince Andrew Pimentel <contact@vincep.me>
2019-01-28 23:13:59 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe Post, type: :model do
 
   it 'has a summary' do
     post = Post.create!(title: "My title", description: "The post description")
-    expect(post).to eq("My title - The post description")
+    expect(post.post_summary).to eq("My title - The post description")
   end
 end
